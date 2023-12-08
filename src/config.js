9779d8f01ebf5c9092cb0cc6e7819418e30ad033
Irfan Maulana <irfan.resimen2018@gmail.com>
2023-12-08 17:55:52 +0700

    Initial Commit

---
+++ src/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAO5flxesvFbBqArjfvHtvLCMSQw-TTxek",
+  authDomain: "next-demo-998ed.firebaseapp.com",
+  projectId: "next-demo-998ed",
+  storageBucket: "next-demo-998ed.appspot.com",
+  messagingSenderId: "179395147585",
+  appId: "1:179395147585:web:d4e70efbf383b5b93613c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
